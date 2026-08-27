078077e1418798d35a4b5a5f105e0b36645afaf4
grizz3 <grizzly476@outlook.com>
2026-08-26 23:59:06 -0500

    Fairly straightforward function that takes a string input, then with a loop outputs the same input in order character by character, so starting at the top, it builds a new string in the reverse order.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
 };
 
 // Do not edit below this line
