dcdcd9a1fa2e26679d2c6bea4cfae575f5090571
grizz3 <grizzly476@outlook.com>
2026-08-27 00:08:29 -0500

    Used the array.filter method to filter out the input items and remove them from the array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...itemsToRemove) => {
+  return array.filter(item => !itemsToRemove.includes(item));
 };
 
 // Do not edit below this line
