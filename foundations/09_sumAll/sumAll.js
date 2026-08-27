c785613f54df8ee3ece1a3c8f2c01d589a8e40bb
grizz3 <grizzly476@outlook.com>
2026-08-27 00:23:00 -0500

    Created a function that first checks that both inputs are integers, and that they aren't negative. Starts the sum at zero, then does a for loop to add the numbers to the current sum.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = (num1, num2) => {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  const min = Math.min(num1, num2);
+  const max = Math.max(num1, num2);
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
