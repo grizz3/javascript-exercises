cfbcf76bc13a28ccce87bbd5df260a477a888659
grizz3 <grizzly476@outlook.com>
2026-08-26 23:41:56 -0500

    Made a basic for loop for running through two inputs. Added checks for things like making sure it's a non-negative whole number, etc.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(input, repeatCount) {
+  if (repeatCount < 0) return 'ERROR';
+  if (typeof input === 'number' && (!Number.isInteger(input) || input < 0)) {
+    return 'ERROR';
+  }
+  let result = '';
+  for (let i = 0; i < repeatCount; i++) {
+    result += input;
+  }
+  return typeof input === 'number' ? Number(result) : result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
